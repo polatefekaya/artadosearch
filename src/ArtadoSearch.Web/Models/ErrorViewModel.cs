fd6fc6f79c487681461d1641168f72a7ed318b2e
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-07-01 17:32:39 +0300

    Initial Commit

---
+++ src/ArtadoSearch.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace ArtadoSearch.Web.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
