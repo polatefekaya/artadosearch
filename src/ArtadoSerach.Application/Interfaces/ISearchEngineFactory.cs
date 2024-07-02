a6b33cfc2c5a6fedbcd938357afde6ca60b9e2da
Polat Efe Kaya <polatsfekaya@gmail.com>
2024-07-02 17:58:19 +0300

    search engine factory init started

---
+++ src/ArtadoSerach.Application/Interfaces/ISearchEngineFactory.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+
+namespace ArtadoSerach.Application.Interfaces {
+    public interface ISearchEngineFactory {
+    }
+}
---
+++ src/ArtadoSerach.Application/Services/SearchEngineFactory.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+
+namespace ArtadoSerach.Application.Services {
+    public class SearchEngineFactory {
+    }
+}
